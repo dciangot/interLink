9cded86d32da6032df7252517406dd8c9a6b179c
Ubuntu <ubuntu@muletto.cloudcnaf>
2025-05-21 15:18:22 +0200

    helm version in ci fix

--- ci/main.go
+++ ci/main.go
@@ -257,7 +257,7 @@ EOF`}).
 			"-n", "interlink",
 			"virtual-node",
 			"oci://ghcr.io/interlink-hq/interlink-helm-chart/interlink",
-			"--version", "0.4.1",
+			"--version", "0.4.2",
 			"--values", "/manifests/vk_helm_chart.yaml",
 		}).Stdout(ctx)
 
